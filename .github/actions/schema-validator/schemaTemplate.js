780883e11cdda2bfc15df39723e4ab3151206e9b
panagiotis megremis <panagiotis.megremis@camelotls.com>
2021-10-11 15:30:42 +0300

    [int-tests] update the schema of npm audit

--- .github/actions/schema-validator/schemaTemplate.js
+++ .github/actions/schema-validator/schemaTemplate.js
@@ -1,249 +1,207 @@
 const npmAuditSchema = {
-  type: 'object',
-  patternProperties: {
-    '^[0-9]*$': {
-      type: 'object',
-      properties: {
-        findings: {
-          type: 'array',
-          items: [
-            {
-              type: 'object',
-              properties: {
-                version: {
-                  type: 'string'
+    type: "object",
+    patternProperties: {
+        '^[0-9]*$': {
+            type: "object",
+            properties: {
+                findings: {
+                    type: "array",
+                    items: [
+                        {
+                            type: "object",
+                            properties: {
+                                version: {
+                                    type: "string"
+                                },
+                                paths: {
+                                    type: "array",
+                                    items: [
+                                        {
+                                            type: "string"
+                                        }
+                                    ]
+                                }
+                            },
+                            required: [
+                                "version",
+                                "paths"
+                            ]
+                        }
+                    ]
                 },
-                paths: {
-                  type: 'array',
-                  items: [
-                    {
-                      type: 'string'
-                    },
-                    {
-                      type: 'string'
-                    },
-                    {
-                      type: 'string'
-                    },
-                    {
-                      type: 'string'
-                    }
-                  ]
+                metadata: {
+                    type: "null"
+                },
+                vulnerable_versions: {
+                    type: "string"
+                },
+                module_name: {
+                    type: "string"
+                },
+                severity: {
+                    type: "string"
+                },
+                github_advisory_id: {
+                    type: "string"
+                },
+                cves: {
+                    type: "array",
+                    items: {}
+                },
+                access: {
+                    type: "string"
+                },
+                patched_versions: {
+                    type: "string"
+                },
+                updated: {
+                    type: "string"
+                },
+                recommendation: {
+                    type: "string"
+                },
+                cwe: {
+                    type: "string"
+                },
+                found_by: {
+                    type: "null"
+                },
+                deleted: {
+                    type: "null"
+                },
+                id: {
+                    type: "integer"
+                },
+                references: {
+                    type: "string"
+                },
+                created: {
+                    type: "string"
+                },
+                reported_by: {
+                    type: "null"
+                },
+                title: {
+                    type: "string"
+                },
+                npm_advisory_id: {
+                    type: "null"
+                },
+                overview: {
+                    type: "string"
+                },
+                url: {
+                    type: "string"
                 }
-              },
-              required: [
-                'version',
-                'paths'
-              ]
-            }
-          ]
-        },
-        found_by: {
-          type: 'object',
-          properties: {
-            link: {
-              type: 'string'
             },
-            name: {
-              type: 'string'
-            }
-          },
-          required: [
-            'link',
-            'name'
-          ]
-        },
-        module_name: {
-          type: 'string'
-        },
-        reported_by: {
-          type: 'object',
-          properties: {
-            link: {
-              type: 'string'
-            },
-            name: {
-              type: 'string'
-            }
-          },
-          required: [
-            'link',
-            'name'
-          ]
-        },
-        cves: {
-          type: 'array',
-          items: {}
-        },
-        references: {
-          type: 'string'
-        },
-        updated: {
-          type: 'string'
-        },
-        id: {
-          type: 'integer'
-        },
-        deleted: {
-          type: 'null'
-        },
-        severity: {
-          type: 'string'
-        },
-        created: {
-          type: 'string'
-        },
-        metadata: {
-          type: 'object',
-          properties: {
-            module_type: {
-              type: 'string'
-            },
-            exploitability: {
-              type: 'integer'
-            },
-            affected_components: {
-              type: 'string'
-            }
-          },
-          required: [
-            'module_type',
-            'exploitability',
-            'affected_components'
-          ]
-        },
-        vulnerable_versions: {
-          type: 'string'
-        },
-        overview: {
-          type: 'string'
-        },
-        cwe: {
-          type: 'string'
-        },
-        patched_versions: {
-          type: 'string'
-        },
-        title: {
-          type: 'string'
-        },
-        recommendation: {
-          type: 'string'
-        },
-        access: {
-          type: 'string'
-        },
-        url: {
-          type: 'string'
+            required: [
+                "findings",
+                "metadata",
+                "vulnerable_versions",
+                "module_name",
+                "severity",
+                "github_advisory_id",
+                "cves",
+                "access",
+                "patched_versions",
+                "updated",
+                "recommendation",
+                "cwe",
+                "found_by",
+                "deleted",
+                "id",
+                "references",
+                "created",
+                "reported_by",
+                "title",
+                "npm_advisory_id",
+                "overview",
+                "url"
+            ]
         }
-      },
-      required: [
-        'findings',
-        'found_by',
-        'module_name',
-        'reported_by',
-        'cves',
-        'references',
-        'updated',
-        'id',
-        'deleted',
-        'severity',
-        'created',
-        'metadata',
-        'vulnerable_versions',
-        'overview',
-        'cwe',
-        'patched_versions',
-        'title',
-        'recommendation',
-        'access',
-        'url'
-      ]
-    }
-  },
-  required: true,
-  additionalProperties: false,
-  id: 'npm'
-};
+    },
+        required: true,
+        additionalProperties: false,
+        id: 'npm'
+    };
 
 const owaspAuditSchema = {
-  type: 'object',
-  patternProperties: {
-    '^[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12}$': {
-  type: 'object',
-      properties: {
-    name: {
-      type: 'string'
-    },
-    desc: {
-      type: 'string'
-    },
-    uri: {
-      type: 'array',
-          items: [
-        {
-          type: 'string'
-        },
-        {
-          type: 'string'
-        },
-        {
-          type: 'string'
-        },
-        {
-          type: 'string'
-        },
-        {
-          type: 'string'
-        },
-        {
-          type: 'string'
-        },
-        {
-          type: 'string'
-        },
-        {
-          type: 'string'
-        },
-        {
-          type: 'string'
-        },
-        {
-          type: 'string'
-        },
-        {
-          type: 'string'
-        },
-        {
-          type: 'string'
+    type: 'object',
+    patternProperties: {
+        '^[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12}$': {
+            type: 'object',
+            properties: {
+                name: {
+                    type: 'string'
+                },
+                desc: {
+                    type: 'string'
+                },
+                uri: {
+                    type: 'array',
+                    items: [
+                        {
+                            type: 'string'
+                        },
+                        {
+                            type: 'string'
+                        },
+                        {
+                            type: 'string'
+                        },
+                        {
+                            type: 'string'
+                        },
+                        {
+                            type: 'string'
+                        },
+                        {
+                            type: 'string'
+                        },
+                        {
+                            type: 'string'
+                        },
+                        {
+                            type: 'string'
+                        },
+                        {
+                            type: 'string'
+                        },
+                        {
+                            type: 'string'
+                        },
+                        {
+                            type: 'string'
+                        },
+                        {
+                            type: 'string'
+                        }
+                    ]
+                },
+                riskdesc: {
+                    type: 'string'
+                },
+                reference: {
+                    type: 'string'
+                },
+                solution: {
+                    type: 'string'
+                }
+            },
+            required: [
+                'name',
+                'desc',
+                'uri',
+                'riskdesc',
+                'reference',
+                'solution'
+            ]
         }
-      ]
     },
-    riskdesc: {
-      type: 'string'
-    },
-    reference: {
-      type: 'string'
-    },
-    solution: {
-      type: 'string'
-    }
-  },
-  required: [
-    'name',
-    'desc',
-    'uri',
-    'riskdesc',
-    'reference',
-    'solution'
-  ]
-}
-},
-  required: true,
-  additionalProperties: false,
-  id: 'owasp'
+    required: true,
+    additionalProperties: false,
+    id: 'owasp'
 };
-
 
 
 module.exports = { npmAuditSchema, owaspAuditSchema };
